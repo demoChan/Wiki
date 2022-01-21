64173b4b7aa8a16d256f4506d1027afe3462f5da
demoChan <979324340@qq.com>
2022-01-21 17:44:10 +0800

    2-2 init

---
+++ src/test/java/com/demo/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
