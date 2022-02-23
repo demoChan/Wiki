bc44c742bb28ed1fb40a83c302405a43ea9c612f
demoChan <979324340@qq.com>
2022-02-23 09:34:37 +0800

    2-6 开发Hello World接口 访问地址：http://127.0.0.1:8880/hello

--- src/main/java/com/demo/wiki/config/WikiApplication.java
+++ src/main/java/com/demo/wiki/config/WikiApplication.java
@@ -1,11 +1,13 @@
-package com.demo.wiki;
+package com.demo.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan(value = "com.demo")
 @SpringBootApplication
 public class WikiApplication {
 
---
+++ src/main/java/com/demo/wiki/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.demo.wiki.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController     //返回字符串
+/* @Controller     //返回页面 */
+public class TestController {
+
+/*
+    * 增删改查GET,POST,PUT,DELETE
+    * @return
+    * 传统风格：/user?id=1
+    * restful风格：/user/1
+    * @RequestMapping接口支持所有请求方式（GetMapping,PostMapping...)
+    */
+
+//    @RequestMapping
+
+//    @PostMapping
+//    @PutMapping
+//    @DeleteMapping
+//    @RequestMapping(value = "hello",method = RequestMethod.GET )   //http请求总共有8种请求方式
+    @GetMapping("/hello")      //405报错时请求method不支持；404表示请求访问不到，没有这个接口
+    public String hello() {
+        return "Hello World!";
+    }
+}
