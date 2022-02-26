4b72418bbd56f2151e4da1c23b3046737d0c4431
demoChan <979324340@qq.com>
2022-02-26 20:44:16 +0800

    3-6 新增ebook表，生成持久层代码

---
+++ src/main/java/com/demo/wiki/controller/EbookController.java
@@ -0,0 +1,24 @@
+package com.demo.wiki.controller;
+
+import com.demo.wiki.domain.Ebook;
+import com.demo.wiki.service.EbookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+
+@RestController     //返回字符串
+@RequestMapping("/ebook")
+public class EbookController {
+
+    @Resource
+    private EbookService ebookService;
+
+    @GetMapping("/list")
+    public List<Ebook> list() {
+        return ebookService.list();
+    }
+}
