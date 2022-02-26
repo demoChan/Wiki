5769dc58df92989f4f8ac9a5a7bbef90ae8ccc2e
demoChan <979324340@qq.com>
2022-02-26 20:55:12 +0800

    3-6 增加通用的返回类CommonResp,以后所有接口的返回值全都是CommonResp类型

--- src/main/java/com/demo/wiki/controller/EbookController.java
+++ src/main/java/com/demo/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.demo.wiki.controller;
 
 import com.demo.wiki.domain.Ebook;
+import com.demo.wiki.resp.CommonResp;
 import com.demo.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,11 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list()
+    {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
