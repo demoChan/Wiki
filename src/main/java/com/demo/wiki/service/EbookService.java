b8773cee64ac809f9cd3f7d0be0134f5df4a1547
demoChan <979324340@qq.com>
2022-03-29 17:49:57 +0800

    6-2 使用PageHelper实现后端分页

--- src/main/java/com/demo/wiki/service/EbookService.java
+++ src/main/java/com/demo/wiki/service/EbookService.java
@@ -5,16 +5,21 @@ import com.demo.wiki.domain.EbookExample;
 import com.demo.wiki.mapper.EbookMapper;
 import com.demo.wiki.req.EbookReq;
 import com.demo.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.demo.wiki.util.CopyUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class EbookService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
 
     @Resource
     private EbookMapper ebookMapper;
@@ -25,15 +30,25 @@ public class EbookService {
         if (!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
+        PageHelper.startPage(1,4);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList){
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}",pageInfo.getTotal());
+        LOG.info("总页数：{}",pageInfo.getPages());
 
-        return respList;
+//        List<EbookResp> respList = new ArrayList<>();
+////        for (Ebook ebook : ebookList){
+////            EbookResp ebookResp = new EbookResp();
+////            BeanUtils.copyProperties(ebook, ebookResp);
+////            //对象复制
+////            EbookResp ebookResp = CopyUtil.copy(ebook,EbookResp.class);
+////            respList.add(ebookResp);
+////        }
+
+        //列表复制
+        List<EbookResp> list = CopyUtil.copyList(ebookList,EbookResp.class);
+
+        return list;
     }
 }
