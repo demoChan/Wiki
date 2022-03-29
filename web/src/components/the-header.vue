7b2e3f4da02ddac4ae55008d093e828d23d1b134
demoChan <979324340@qq.com>
2022-03-29 15:36:54 +0800

    6-1 增加文献管理页面

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -5,14 +5,14 @@
                 mode="horizontal"
                 :style="{ lineHeight: '64px' }"
         >
-            <a-menu-item key="1">
-                nav 11
+            <a-menu-item key="/">
+                <router-link to="/">首页</router-link>
             </a-menu-item>
-            <a-menu-item key="2">
-                nav 2
+            <a-menu-item key="/admin/ebook">
+                <router-link to="/admin/ebook">文献管理</router-link>
             </a-menu-item>
-            <a-menu-item key="3">
-                nav 3
+            <a-menu-item key="/about">
+                <router-link to="/about">关于系统</router-link>
             </a-menu-item>
         </a-menu>
     </a-layout-header>
