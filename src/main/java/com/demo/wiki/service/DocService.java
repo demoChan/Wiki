06ebb26192d456b04765015544e1e7b323ec3f5b
demoChan <979324340@qq.com>
2022-04-12 16:39:47 +0800

    8-9 增加保存文档内容功能

--- src/main/java/com/demo/wiki/service/DocService.java
+++ src/main/java/com/demo/wiki/service/DocService.java
@@ -1,7 +1,9 @@
 package com.demo.wiki.service;
 
+import com.demo.wiki.domain.Content;
 import com.demo.wiki.domain.Doc;
 import com.demo.wiki.domain.DocExample;
+import com.demo.wiki.mapper.ContentMapper;
 import com.demo.wiki.mapper.DocMapper;
 import com.demo.wiki.req.DocQueryReq;
 import com.demo.wiki.req.DocSaveReq;
@@ -26,6 +28,9 @@ public class DocService {
 
     @Resource
     private DocMapper docMapper;
+
+    @Resource
+    private ContentMapper contentMapper;
 
     @Resource
     private SnowFlake snowFlake;
@@ -75,13 +80,21 @@ public class DocService {
      */
     public void save(DocSaveReq req) {
         Doc doc = CopyUtil.copy(req, Doc.class);
+        Content content = CopyUtil.copy(req, Content.class);
         if (ObjectUtils.isEmpty(req.getId())) {
             // 新增
             doc.setId(snowFlake.nextId());
             docMapper.insert(doc);
+
+            content.setId(doc.getId());
+            contentMapper.insert(content);
         } else {
             // 更新
             docMapper.updateByPrimaryKey(doc);
+            int count = contentMapper.updateByPrimaryKeyWithBLOBs(content);
+            if(count == 0){
+                contentMapper.insert(content);
+            }
         }
     }
 
