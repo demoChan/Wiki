8430f3e82d12b05c34ae18c1e8dce3084ffa381b
demoChan <979324340@qq.com>
2022-05-02 19:46:27 +0800

    9-11 集成redis，登录成功后将登录信息放入redis中

--- src/main/java/com/demo/wiki/controller/UserController.java
+++ src/main/java/com/demo/wiki/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.demo.wiki.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.demo.wiki.req.UserLoginReq;
 import com.demo.wiki.req.UserQueryReq;
 import com.demo.wiki.req.UserResetPasswordReq;
@@ -9,19 +10,33 @@ import com.demo.wiki.resp.PageResp;
 import com.demo.wiki.resp.UserLoginResp;
 import com.demo.wiki.resp.UserQueryResp;
 import com.demo.wiki.service.UserService;
+import com.demo.wiki.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 
 @RestController     //返回字符串
 @RequestMapping("/user")
 public class UserController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
     @Resource
     private UserService userService;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
 
     @GetMapping("/list")
     public CommonResp list(@Valid UserQueryReq req)
@@ -63,6 +78,11 @@ public class UserController {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(req);
+
+        Long token = snowFlake.nextId();
+        LOG.info("生成单点登录token：{}，并放入redis中", token);
+        userLoginResp.setToken(token.toString());
+        redisTemplate.opsForValue().set(token.toString(), JSONObject.toJSONString(userLoginResp), 3600 * 24, TimeUnit.SECONDS);
         resp.setContent(userLoginResp);
         return resp;
     }
