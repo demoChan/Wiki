eb3e5f5678262258038fa86c2aa56cc2d47b4269
demoChan <979324340@qq.com>
2022-05-02 18:30:30 +0800

    9-10 完成基本登录功能

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -45,13 +45,18 @@
 
 <script lang="ts">
     import { defineComponent, ref } from 'vue';
+    import axios from 'axios';
+    import {message} from 'ant-design-vue';
+
+    declare let hexMd5: any;
+    declare let KEY: any;
 
     export default defineComponent({
         name: 'the-header',
         setup () {
             const loginUser = ref({
-                loginName: "test",
-                password: "test"
+                loginName: "",
+                password: ""
             });
             const loginModalVisible = ref(false);
             const loginModalLoading = ref(false);
@@ -61,7 +66,19 @@
 
             // 登录
             const login = () => {
-                console.log("开始登录")
+                console.log("开始登录");
+                loginModalLoading.value = true;
+                loginUser.value.password = hexMd5(loginUser.value.password + KEY);
+                axios.post('/user/login', loginUser.value).then((response) => {
+                    loginModalLoading.value = false;
+                    const data = response.data;
+                    if (data.success) {
+                        loginModalVisible.value = false;
+                        message.success("登录成功！");
+                    }else {
+                        message.error(data.message);
+                    }
+                });
             };
 
             return {
